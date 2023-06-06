5174a6f4ce3ce438be286f9270186dc0db76cfc1
Sarthak Singh Tomar <singhtomarsarthak@gmail.com>
2023-06-06 23:39:50 +0530

    initial commit

---
+++ apiapplication/src/test/java/com/application/apiapplication/ApiapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.apiapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
