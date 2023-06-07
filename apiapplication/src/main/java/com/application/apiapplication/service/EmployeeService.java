318021da3e093d3dc0b854ded520d9870c489299
Sarthak Singh Tomar <singhtomarsarthak@gmail.com>
2023-06-08 01:27:17 +0530

    second commit

--- apiapplication/src/main/java/com/application/apiapplication/service/EmployeeService.java
+++ apiapplication/src/main/java/com/application/apiapplication/service/EmployeeService.java
@@ -1,15 +1,24 @@
 package com.application.apiapplication.service;
 
-public class EmployeeService {
+import com.application.apiapplication.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
 
     /*SPECIFICATIONS*/
     // save an employee
+    void addEmployee(Employee employee);
 
     // get employees
+    List<Employee> getAllEmployees();
 
     // get one employee
+    Employee getEmployeeById(Integer id);
 
     // update employee
+    void updateEmployee(Employee employee);
 
     // delete employee
+    Boolean deleteEmployeeById(Integer id);
 }
