d44384775321cba252543f41ce41e3dc1e609235
Sarthak Singh Tomar <singhtomarsarthak@gmail.com>
2023-06-07 22:14:50 +0530

    first commit

--- apiapplication/src/main/java/com/application/apiapplication/APIApplication.java
+++ apiapplication/src/main/java/com/application/apiapplication/APIApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiapplicationApplication {
+public class APIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiapplicationApplication.class, args);
+		SpringApplication.run(APIApplication.class, args);
 	}
 
 }
---
+++ apiapplication/src/main/java/com/application/apiapplication/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.application.apiapplication.service;
+
+public class EmployeeService {
+
+    /*SPECIFICATIONS*/
+    // save an employee
+
+    // get employees
+
+    // get one employee
+
+    // update employee
+
+    // delete employee
+}
